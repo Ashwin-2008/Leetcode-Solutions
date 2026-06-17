b053d6f18f912fd2d377b7c176552d79ddf257c7
Ashwin <ashwinsridharan71@gmail.com>
2026-06-17 14:25:28 +0530

    Time: 1 ms (95.42%), Space: 42.4 MB (68.95%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int s=0;
+        int ss=0;
+        while(n>0){
+            s+=n%10;
+            ss+=(n%10)*(n%10);
+            n/=10;
+        }
+        return ss-s>=50;
+    }
+}
